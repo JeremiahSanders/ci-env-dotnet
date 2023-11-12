66e68f7b0fd45e3620a3c0cc08d4adf2dd3cb737
Jeremiah Sanders <1328897+JeremiahSanders@users.noreply.github.com>
2023-11-12 13:50:11 -0600

    fix: execute 'latest' tagging during build

--- ci/libexec/actions/docker_publish_xarch_image.sh
+++ ci/libexec/actions/docker_publish_xarch_image.sh
@@ -5,20 +5,33 @@ set -o nounset  # Fail if an unset variable is used.
 set -o pipefail # Fail pipelines if any command errors, not just the last one.
 
 function docker_publish_xarch_image() {
-  function _push() {
+  function __push() {
+    function __conditionallyPushLatest() {
+      if [[ "${RELEASE_ENVIRONMENT:-false}" = true ]]; then
+        docker buildx build \
+          --file "${PROJECT_ROOT}/Dockerfile" \
+          --tag "${DOCKER_IMAGE}" \
+          --tag "${DOCKER_IMAGE_REPOSITORY}:latest" \
+          --platform linux/amd64,linux/arm64 \
+          --push .
+      else
+        docker buildx build \
+          --file "${PROJECT_ROOT}/Dockerfile" \
+          --tag "${DOCKER_IMAGE}" \
+          --platform linux/amd64,linux/arm64 \
+          --push .
+      fi
+    }
     printf "Preparing to push %s.\n\n" "${DOCKER_IMAGE}" &&
-      docker buildx build \
-        --file "${PROJECT_ROOT}/Dockerfile" \
-        --tag "${DOCKER_IMAGE}" \
-        --platform linux/amd64,linux/arm64 \
-        --push . &&
+      __conditionallyPushLatest &&
       printf "\n\nPushed combined docker image %s.\n\n" "${DOCKER_IMAGE}"
   }
 
   docker_apply_environment &&
     require-var "DOCKER_IMAGE" &&
+    require-var "DOCKER_IMAGE_REPOSITORY" &&
     docker_use_project_context &&
-    _push
+    __push
 }
 
 export -f docker_publish_xarch_image
